53a2f1995083c80aea8d9e0605714d85e76fcf37
Szymon Kedra <s5zee@wp.pl>
2023-01-31 19:28:53 +0100

    focus added on id change, useRef added

--- src/common/Input/index.tsx
+++ src/common/Input/index.tsx
@@ -1,20 +1,31 @@
 import { useQueryParameter } from "../../useQueryParameter";
 import { useReplaceQueryParameter } from "../../useReplaceQueryParameter";
 import { Header, IdInput, InputContainer } from "./styled";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export const Input = () => {
   const replaceQueryParameter = useReplaceQueryParameter();
   const id = useQueryParameter("id");
 
+  const ref = useRef<HTMLInputElement>(null);
+
+  const focusRef = () => {
+    ref.current?.focus();
+  };
+
   const filterId = (event: React.ChangeEvent<HTMLInputElement>) => {
     replaceQueryParameter({ key: "id", value: event.target.value });
   };
+
+  useEffect(() => {
+    focusRef();
+  }, [id]);
 
   return (
     <InputContainer>
       <Header>Filter table</Header>
       <IdInput
+        ref={ref}
         onChange={filterId}
         value={id || ""}
         type="number"
