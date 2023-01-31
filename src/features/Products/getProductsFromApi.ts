0aebcbcfebe0c21c7aea790975a5977a061eb01d
Szymon Kedra <s5zee@wp.pl>
2023-01-31 14:52:46 +0100

    typed Product and getProductsFromApi function

--- src/features/Products/getProductsFromApi.ts
+++ src/features/Products/getProductsFromApi.ts
@@ -1,8 +1,19 @@
+export interface Product {
+  id: number;
+  name: string;
+  year: number;
+  color: string;
+  pantone_value: string;
+  data: [];
+  page: number;
+  total_pages: number;
+}
+
 export const getProductsFromApi = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
     new Error(response.statusText);
   }
 
-  return await response.json();
+  return (await response.json()) as Promise<Product[]>;
 };
