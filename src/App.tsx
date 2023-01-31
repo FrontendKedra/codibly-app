1b2634113e42335ed2c8dfe2604e2ac9b2adebb8
Szymon Kedra <s5zee@wp.pl>
2023-01-31 11:51:34 +0100

    Main routing added, index.js changed to ts

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { AppContainer } from "./features/AppContainer/styled";
+import { ViewRenderer } from "./features/ViewRenderer";
+
+function App() {
+  return (
+    <HashRouter>
+      <AppContainer>
+        <Switch>
+          <Route path="/main">
+            <ViewRenderer />
+          </Route>
+          <Route path="/">
+            <Redirect to="/main" />
+          </Route>
+        </Switch>
+      </AppContainer>
+    </HashRouter>
+  );
+}
+
+export default App;
