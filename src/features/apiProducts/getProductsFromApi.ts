ab6e29650ed159987f5d092d562716bb878aec6d
Szymon Kedra <s5zee@wp.pl>
2023-02-19 18:40:13 +0100

    throw word added for better performance

--- src/features/apiProducts/getProductsFromApi.ts
+++ src/features/apiProducts/getProductsFromApi.ts
@@ -9,7 +9,7 @@ export interface Product {
 export const getProductsFromApi = async (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
-    new Error(response.statusText);
+    throw new Error(response.statusText);
   }
 
   return (await response.json()) as Promise<Product[]>;
