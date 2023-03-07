bf7afa4a904f2f5fba36fa0b51e931981a140b07
Szymon Kedra <s5zee@wp.pl>
2023-03-07 23:26:51 +0100

    interfaces for the entire api Added

--- src/common/interfaces/interface.ts
+++ src/common/interfaces/interface.ts
@@ -1,7 +1,21 @@
 export interface Data {
-    id: number;
-    name: string;
-    year: number;
-    color: string;
-    pantone_value: string;
-  }
+  id: number;
+  name: string;
+  year: number;
+  color: string;
+  pantone_value: string;
+}
+
+export interface Support {
+  url: string;
+  text: string;
+}
+
+export interface ApiResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: Data[];
+  support: Support;
+}
