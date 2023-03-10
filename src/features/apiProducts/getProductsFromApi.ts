251414b9118314a08288806ea4a99df73ca88eea
Szymon Kedra <s5zee@wp.pl>
2023-03-10 23:25:24 +0100

    formatting

--- src/features/apiProducts/getProductsFromApi.ts
+++ src/features/apiProducts/getProductsFromApi.ts
@@ -2,6 +2,7 @@ import { ApiResponse } from "../../common/interfaces/interface";
 
 export const getProductsFromApi = async (url: string) => {
   const response = await fetch(url);
+
   if (!response.ok) {
     throw new Error(response.statusText);
   }
